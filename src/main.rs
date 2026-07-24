decf84a62e65403ddb1d7eb9c7b2b6be39254769
for-lack-of-a-better-name-j <168361532+for-lack-of-a-better-name-j@users.noreply.github.com>
2026-07-24 06:13:34 -0400

    feat: allocate_humongous_tensor

--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,32 @@ fn trigger_overflow_burn_multiple_tensors<B: Backend>(device: &B::Device) {
     let raw_data = computation.into_data();
     println!("raw_data: {:?}", raw_data);
 }
+fn allocate_humongous_tensor<B: Backend>(device: &B::Device) {
+    let shape = [1_250_000_000]; // 1,250,000,000 f32s * 4 bytes = 5.0gb,
+    println!("Creating humongous tensor!");
+    let humongous_data = TensorData::ones::<f32, _>(shape);
+    let humongous_tensor = Tensor::<B, 1>::from_data(humongous_data, device);
+    println!("Creating itty bitty tensor!");
+    let itty_bitty_shape = [1]; //
+    let mut itty_bitty_tensors = Vec::new();
+    for i in 0..3 {
+        println!("creating itty_bitty_tensor {i}");
+        let itty_bitty_data = TensorData::ones::<f32, _>([1]);
+        let itty_bitty_tensor = Tensor::<B, 1>::from_data(itty_bitty_data, device);
+        itty_bitty_tensors.push(itty_bitty_tensor.clone());
+    }
+
+    println!("chain some ops so that they are sent to GPU and show large allocation behavior!");
+    let mut computation = humongous_tensor.clone();
+    for itty_bitty_tensor in itty_bitty_tensors.iter() {
+        computation = computation * itty_bitty_tensor.clone();
+    }
+    let raw_data = computation.into_data();
+    println!("raw_data: {:?}", raw_data);
+}
 fn main() {
     type RocmBackend = Autodiff<Rocm>;
     let device = RocmDevice::default();
+    allocate_humongous_tensor::<RocmBackend>(&device);
     trigger_overflow_burn_multiple_tensors::<RocmBackend>(&device);
 }
